572092d1f2045a8b1c157c98d1acedc8cca3ac94
Arpit <arpit_jain@live.com>
2021-12-11 14:54:56 +0530

    Adds mongo connection using mongodb driver.

--- app.js
+++ app.js
@@ -1,41 +1,71 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const { MongoClient } = require("mongodb");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// database connection
+const mongoUri =
+  "mongodb+srv://learning:learning123@learning.zejua.mongodb.net/mongodb?retryWrites=true&w=majority";
+const client = new MongoClient(mongoUri);
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.get("/patients", async (req, res, next) => {
+  try {
+    const connection = await client.connect()
+    console.log(`Successfully connected to the database`);
+    const db = await connection.db("mongodb");
+    const patients = await db.collection("patients").find({}).toArray();
+
+    // db.collection("patients").insertOne()
+    // db.collection("patients").insertMany()
+    // db.collection("patients").find()
+    // db.collection("patients").findOne()
+    // db.collection("patients").updateOne()
+    // db.collection("patients").updateMany()
+    // db.collection("patients").deleteOne()
+    // db.collection("patients").deleteMany()
+
+    return res.json(patients)
+  } catch (error) {
+    console.error('Error', error);
+    res.send("Not okay");
+  } finally {
+    client.close();
+  }
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
